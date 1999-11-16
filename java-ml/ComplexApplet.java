272a0fcb65efcc574ce4c1801e9493ff57028998
gjb <>
1999-11-16 01:26:01 +0000

    Clean up some errors-- let ComplexAppletWithErrors.java contain errors and let this one be okay.

--- java-ml/ComplexApplet.java
+++ java-ml/ComplexApplet.java
@@ -16,8 +16,11 @@ public abstract class ComplexApplet extends Applet {
   // this method displays the applet.
   // the Graphics class is how you do all the drawing in Java
   public void paint(Graphics g) {
+    String foo;
     int i = 64, j;
-    g.drawString("Hello World", 25, 50);
+    if (i == 22) {
+      g.drawString("Hello World", 25, 50);
+    }
     System.err.println(Math.sqrt(49));
     System.err.println(Math.sqrt(i));
     System.err.println(r[1]);
@@ -29,15 +32,22 @@ public abstract class ComplexApplet extends Applet {
     for (int i = 0; i < 10; ++i);
     for (int i = 0; i < 10; ++i) {
       System.out.println(i);
+      if (i == 11) {
+        return new Double(2);
+      }
     }
     return o;
   }
 
   private int wacky(final Graphics g) {
+    String strTest;
     return 5;
   }
 
-  protected final int bar() {
+  protected final int bar(int k) {
+    if (k > 0) {
+      return 5;
+    } 
     return 1 + 2 + 3;
   }
   double baz(int i) throws Exception {
